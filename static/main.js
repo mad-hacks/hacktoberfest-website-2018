53adc44f2aeaf5db71baff2964b52b5484f9e48c
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-09-29 13:52:35 +0530

    Fixes for mobile screens :construction:

--- static/main.js
+++ static/main.js
@@ -1,32 +1,32 @@
 // Count Down Date
-var countDownDate = new Date("Oct 1, 2018 00:00:00").getTime();
+let countDownDate = new Date("Oct 2, 2018 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let x = setInterval(() => {
 
     // Get todays date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
     
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    let distance = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     if (days < 10) {
         Str = days.toString();
         days = Str.padStart(2, "0");
     }
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     if (hours < 10) {
         Str = hours.toString();
         hours = Str.padStart(2, "0");
     }
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     if (minutes < 10) {
         Str = minutes.toString();
         minutes = Str.padStart(2, "0");
     }
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     if (seconds < 10) {
         Str = seconds.toString();
         seconds = Str.padStart(2, "0");
@@ -42,12 +42,12 @@ var x = setInterval(function() {
     }
 }, 1000);
 
-document.getElementById("fb").addEventListener("mouseover", function(){document.getElementById("fb").src="static/images/fbb2.png";})
-document.getElementById("fb").addEventListener("mouseout", function(){document.getElementById("fb").src="static/images/fbb.png";})
+document.getElementById("fb").addEventListener("mouseover", () => {document.getElementById("fb").src="static/images/fbb2.png";})
+document.getElementById("fb").addEventListener("mouseout", () => {document.getElementById("fb").src="static/images/fbb.png";})
 
-document.getElementById("twit").addEventListener("mouseover", function(){document.getElementById("twit").src="static/images/twitter2.png";})
-document.getElementById("twit").addEventListener("mouseout", function(){document.getElementById("twit").src="static/images/twitter.png";})
+document.getElementById("twit").addEventListener("mouseover", () => {document.getElementById("twit").src="static/images/twitter2.png";})
+document.getElementById("twit").addEventListener("mouseout", () => {document.getElementById("twit").src="static/images/twitter.png";})
 
-document.getElementById("lin").addEventListener("mouseover", function(){document.getElementById("lin").src="static/images/lin2.png";})
-document.getElementById("lin").addEventListener("mouseout", function(){document.getElementById("lin").src="static/images/lin.png";})
+document.getElementById("lin").addEventListener("mouseover", () => {document.getElementById("lin").src="static/images/lin2.png";})
+document.getElementById("lin").addEventListener("mouseout", () => {document.getElementById("lin").src="static/images/lin.png";})
 
